4a8b2f7b477318a0d6b58d83a1c343eb39938006
melbaylon <3905874+melbaylon@users.noreply.github.com>
2021-01-26 20:05:26 +0800

    added comments to the youtube-dl formulas

--- dl.sh
+++ dl.sh
@@ -1,12 +1,12 @@
 #!usr/bin/zsh
 
 # youtube-dl formula
-
+# Feel free to change this depending on the formats and resolution you want to download
+# This is optimized for downloading WEBM videos
 alias yd="youtube-dl -f '(webm[width <= 3840]) + (251/140/139)' -o '%(title)s - %(id)s - %(uploader)s - %(upload_date)s - %(resolution)s.%(ext)s' --all-subs --embed-subs --add-metadata -i"
 
-#unspecifed formats - works better for downloading non webm video files (fallback)
+# This runs if the preferred formats, set by yd (above) is not available
 alias ydb="youtube-dl -o '%(title)s - %(id)s - %(uploader)s - %(upload_date)s - %(resolution)s.%(ext)s' --all-subs --embed-subs --add-metadata -i"
-
 
 # where you want to save the downloaded files (target folder)
 tf="wjsn_mera"
