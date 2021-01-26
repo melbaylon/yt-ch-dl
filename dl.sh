743dc577ef9c18c0e89e4bb53e8cad351d045102
melbaylon <3905874+melbaylon@users.noreply.github.com>
2021-01-26 19:59:31 +0800

    added youtube-dl fallback (ydb)

--- dl.sh
+++ dl.sh
@@ -1,14 +1,19 @@
 #!usr/bin/zsh
 
 # youtube-dl formula
-alias yd="youtube-dl -f 'webm[width <= 3840] + (251/140/139)' -o '%(title)s - %(id)s - %(uploader)s - %(upload_date)s - %(resolution)s.%(ext)s' --all-subs --embed-subs --add-metadata -i"
+
+alias yd="youtube-dl -f '(webm[width <= 3840]) + (251/140/139)' -o '%(title)s - %(id)s - %(uploader)s - %(upload_date)s - %(resolution)s.%(ext)s' --all-subs --embed-subs --add-metadata -i"
+
+#unspecifed formats - works better for downloading non webm video files (fallback)
+alias ydb="youtube-dl -o '%(title)s - %(id)s - %(uploader)s - %(upload_date)s - %(resolution)s.%(ext)s' --all-subs --embed-subs --add-metadata -i"
+
 
 # where you want to save the downloaded files (target folder)
 tf="wjsn_mera"
 
 echo "" >> ids | cat ids | while read id; \
 do \
-    yd https://youtu.be/$id; \
+    yd https://youtu.be/$id || ydb https://youtu.be/$id; \
     ls | grep -i webm | while read video; do rclone copy $video redu:${tf} -P -v; rm $video; done
     ls | grep -i mkv | while read video; do rclone copy $video redu:${tf} -P -v; rm $video; done
     ls | grep -i mp4 | while read video; do rclone copy $video redu:${tf} -P -v; rm $video; done
