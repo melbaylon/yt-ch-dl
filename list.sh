0c4c59188f1f316ff809dd3bcecea633da0f19fc
melbaylon <3905874+melbaylon@users.noreply.github.com>
2021-01-26 21:08:11 +0800

    added comments to channel and link to cs50 yt ch

--- list.sh
+++ list.sh
@@ -1,4 +1,9 @@
-channel="https://www.youtube.com/channel/UCwS6H7jX50ldmB_IHxECvKw/videos"
+# Link to the channel
+# you want to download from.
+# You should be on the VIDEOS tab
+# and the not on the HOME tab
+# when copying the link.
+channel="https://www.youtube.com/c/cs50/videos"
 
 # youtube-dl $channel --skip-download --match-title  '(아린 | 유아 | 비니)' --min-sleep-interval 5 --max-sleep-interval 10  --get-id -v -i | cat >> ids
 youtube-dl $channel --skip-download --match-title  '(우주소녀)' --min-sleep-interval 5 --max-sleep-interval 10  --get-id -v -i | cat >> ids
