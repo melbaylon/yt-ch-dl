780e2e550c2beb6d10ebce5a7863b7557cce58da
melbaylon <3905874+melbaylon@users.noreply.github.com>
2021-01-26 21:17:49 +0800

    comments about filter and lister

--- list.sh
+++ list.sh
@@ -1,9 +1,20 @@
-# Link to the channel
+# CHANNEL
 # you want to download from.
 # You should be on the VIDEOS tab
 # and the not on the HOME tab
 # when copying the link.
 channel="https://www.youtube.com/c/cs50/videos"
 
-# youtube-dl $channel --skip-download --match-title  '(아린 | 유아 | 비니)' --min-sleep-interval 5 --max-sleep-interval 10  --get-id -v -i | cat >> ids
-youtube-dl $channel --skip-download --match-title  '(우주소녀)' --min-sleep-interval 5 --max-sleep-interval 10  --get-id -v -i | cat >> ids
+# FILTER
+# This limits that video ids
+# that is grabbed from the youtube channel.
+# Multiple filters, example: '(Python|JavaScript|HTML)'
+filter='(Python)'
+
+# LISTER
+# This goes through the videos
+# of the youtube channel.
+# If any part of the video title
+# matches any of the filters,
+# its video is added/appended to the ids file.
+youtube-dl $channel --skip-download --match-title $filter --min-sleep-interval 5 --max-sleep-interval 10  --get-id -v -i | cat >> ids
