c1d8f4b41e55b723d4f4a8ca76f4b7fa6c3ab857
melbaylon <3905874+melbaylon@users.noreply.github.com>
2021-01-26 10:36:33 +0800

    Update dl.sh
    added comments
    added support for mp4 and mkv
    change how each file format is processed

--- dl.sh
+++ dl.sh
@@ -1,10 +1,15 @@
 #!usr/bin/zsh
 
-alias yd="youtube-dl -f 'webm[width <= 3840] + 251' -o '%(title)s - %(id)s - %(uploader)s - %(upload_date)s - %(resolution)s.%(ext)s' --all-subs --embed-subs --add-metadata -i"
+# youtube-dl formula
+alias yd="youtube-dl -f 'webm[width <= 3840] + (251/140/139)' -o '%(title)s - %(id)s - %(uploader)s - %(upload_date)s - %(resolution)s.%(ext)s' --all-subs --embed-subs --add-metadata -i"
+
+# where you want to save the downloaded files (target folder)
+tf="wjsn_mera"
 
 echo "" >> ids | cat ids | while read id; \
 do \
     yd https://youtu.be/$id; \
-    rclone copy *webm redu:wjsn_mera -P -v; \
-    rm *webm; \
+    ls | grep -i webm | while read video; do rclone copy $video redu:${tf} -P -v; rm $video; done
+    ls | grep -i mkv | while read video; do rclone copy $video redu:${tf} -P -v; rm $video; done
+    ls | grep -i mp4 | while read video; do rclone copy $video redu:${tf} -P -v; rm $video; done
 done
